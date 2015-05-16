ff25c6c5f6713e14e51f9b2286d83b0daf173550
Aaron Browder <aaronbrowder@gmail.com>
2015-05-16 17:36:44 +0000

    Support sass compiling

--- client/javascript/client.mockup.js
+++ client/javascript/client.mockup.js
@@ -4,9 +4,7 @@ var client = (function () {
 
 	var initModule = function ($root) {
 		var catHtml = Handlebars.templates.cat({ text: 'meow' });
-		var dogHtml = Handlebars.templates.dog({ text: 'woof' });
-		var cowHtml = Handlebars.templates.cow({ text: 'moo' });
-	  $root.html(catHtml).append(dogHtml).append(cowHtml);
+	  $root.html(catHtml);
 	};
 	
 	return { 
--- server.mockup.js
+++ server.mockup.js
@@ -1,18 +1,26 @@
 var http = require('http');
 var path = require('path');
 var express = require('express');
+var sassMiddleware = require('node-sass-middleware');
 var handlebarsPrecompiler = require('handlebars-precompiler');
 
 var router = express();
 var server = http.createServer(router);
 
-router.use(express.static(path.resolve(__dirname, 'client')));
-
 handlebarsPrecompiler.watchDir(
   path.resolve(__dirname, "client/templates"),
-  path.resolve(__dirname, "client/js/templates.js"),
-  ['handlebars']
+  path.resolve(__dirname, "client/javascript/templates.js"),
+  ['handlebars', 'hbs']
 );
+
+router.use(sassMiddleware({
+    src: path.resolve(__dirname, 'client/stylesheets'),
+    dest: path.resolve(__dirname, 'client/stylesheets/compiled'),
+    debug: true,
+    prefix: '/stylesheets/compiled'
+}));
+
+router.use(express.static(path.resolve(__dirname, 'client')));
 
 server.listen(process.env.PORT || 3000, process.env.IP || "0.0.0.0", function(){
   var addr = server.address();
